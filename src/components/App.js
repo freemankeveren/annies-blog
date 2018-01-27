3a28c5f13e40024926768037eaa16b94ba2df81a
Keveren Freeman <freemankeveren@gmail.com>
2018-01-26 17:15:24 -0700

    init

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+        <h1> "Annie's Blog"</h1>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
