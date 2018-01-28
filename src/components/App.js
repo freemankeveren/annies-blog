49990c017a3d44a6b246e50072d4fa37b3ba3634
Keveren Freeman <freemankeveren@gmail.com>
2018-01-27 17:58:59 -0700

    started nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './App.css';
+import '../App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Recent BLogs</li>
+          <li>All Blogs</li>
+        </ul>
+      </nav>
+      <div>
         <header>
         <h1> "Annie's Blog"</h1>
 
         </header>
+      </div>
       </div>
     );
   }
